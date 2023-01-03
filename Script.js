10802987c76d02ef8aef1e25c0fafb69def6e112
AlexEmrik <alexander.karolin@hotmail.com>
2023-01-03 11:41:05 +0100

    merit sida gjord

--- Script.js
+++ Script.js
@@ -1,6 +1,40 @@
 const showBtn = document.querySelector('.navBtn');
 const topNav = document.querySelector('.top-nav');
 
+function addInfo1() {
+    const elem = document.getElementById('merit1')
+    if (elem.childNodes.length == '') {
+        document.getElementById('merit1').innerHTML = 'Bussen är fotad vid lunda depå';
+        document.getElementById("merit1").style.maxHeight = "10000px";
+    }
+    else{
+        document.getElementById('merit1').innerHTML = '';
+        document.getElementById("merit1").style.maxHeight = "0px"   ;
+    }
+   }
+   function addInfo2() {
+    const elem = document.getElementById('merit2')
+    if (elem.childNodes.length == '') {
+        document.getElementById('merit2').innerHTML = 'Hunden är fotad vid Ekerö depå';
+        document.getElementById("merit2").style.maxHeight = "10000px";
+    }
+    else{
+        document.getElementById('merit2').innerHTML = '';
+        document.getElementById("merit2").style.maxHeight = "0px"   ;
+    }  
+}
+   function addInfo3() {
+    const elem = document.getElementById('merit3')
+    if (elem.childNodes.length == '') {
+        document.getElementById('merit3').innerHTML = 'Bilden är på Ekerö depån/garaget';
+        document.getElementById("merit3").style.maxHeight = "10000px";
+    }
+    else{
+        document.getElementById('merit3').innerHTML = '';
+        document.getElementById("merit3").style.maxHeight = "0px"   ;
+    }
+}
+
 showBtn.addEventListener('click', function(){
     if(topNav.classList.contains('showNav')){
         topNav.classList.remove('showNav');
@@ -12,3 +46,5 @@ showBtn.addEventListener('click', function(){
 });
 
 var lightbox = new SimpleLightbox('.gallery a', { /* options */ });
+
+
